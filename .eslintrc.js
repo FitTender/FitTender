6529dbe020edee3f1b235715ca4d6b9e60ac909b
SOHYEON00 <ohsohyeon24@gmail.com>
2022-10-30 18:57:49 +0900

    feat: eslint, prettier 적용 (노션 공유문서 확인 필요)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,18 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier', '@react-native-community'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
+        '@typescript-eslint/no-empty-interface': 0,
+        '@typescript-eslint/explicit-module-boundary-types': 0,
+        '@typescript-eslint/no-empty-function': 0,
+        '@typescript-eslint/no-explicit-any': 2,
+        '@typescript-eslint/no-unused-vars': 0,
+        '@typescript-eslint/no-var-requires': 0,
       },
     },
   ],
